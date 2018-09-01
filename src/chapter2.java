cf3db78aaa50e246cc755cb324d758a5c18a8ed1
oVokick <ovoeins@yandex.ru>
2018-09-01 20:12:03 +0300

    chapter 3.1

--- src/chapter2.java
+++ src/chapter2.java
@@ -3,7 +3,6 @@
 public class chapter2 {
     public static void main(String[] args){
         System.out.print("2 ");
-
         for (int i = 3; i <= 200; i++){
             for (int y = 2; y < i; y++){
                 if (i % y == 0){
---
+++ src/chapter3.java
@@ -0,0 +1,20 @@
+/*chapter3.1
+Программа, которая получает символы, введенные с клавиатуры, до тех пор, пока не встретится точка.
+Сведения о количестве пробелов выводятся в конце программы.
+ */
+public class chapter3 {
+    public static void main(String[] args)
+            throws java.io.IOException {
+
+        char entered_symbol;
+        char cl;
+        int num = 0;
+
+        for(;;){
+            entered_symbol = (char) System.in.read();
+            if (entered_symbol == '.') break;
+            if (entered_symbol == ' ') num++;
+        }
+        System.out.print(num);
+    }
+}
