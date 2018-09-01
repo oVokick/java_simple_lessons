7d10fdc627ecf0b53e934d0d6822f58b5c6f8d36
oVokick <ovoeins@yandex.ru>
2018-09-01 22:29:02 +0300

    Tasks from chapter 3.

---
+++ src/chapter3_10.java
@@ -0,0 +1,22 @@
+public class chapter3_10 {
+    public static void main(String[] args)
+            throws java.io.IOException {
+        int changed = 0;
+        char symbol;
+
+        do {
+            symbol = (char) System.in.read();
+            if (symbol >= 'a' & symbol <= 'z'){
+                symbol -= 32;
+                System.out.println(symbol);
+                changed++;
+            }
+            else if (symbol >= 'A' & symbol <= 'Z'){
+                symbol += 32;
+                System.out.println(symbol);
+                changed++;
+            }
+        } while (symbol != '.');
+        if (symbol == '.') System.out.println("Symbols changed: " + changed);
+    }
+}
---
+++ src/chapter3_9.java
@@ -0,0 +1,11 @@
+public class chapter3_9 {
+    public static void main(String[] args) {
+        int num = 1;
+
+        for (;;){
+            System.out.print(num + " ");
+            num *= 2;
+            if (num > 1000) break;
+        }
+    }
+}
